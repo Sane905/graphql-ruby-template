707d8b28157d441d34e194e34aa64299def7d177
Sane905 <konjaku0905@yahoo.co.jp>
2023-05-03 13:05:48 +0900

    fix:テーブルのロールバック

--- db/migrate/20230317113529_create_payments.rb
+++
@@ -1,9 +0,0 @@
-class CreatePayments < ActiveRecord::Migration[6.1]
-  def change
-    create_table :payments, id: false do |t|
-      t.string :uid, null: false, primary_key: true
-      t.integer :amount, null: false
-      t.timestamps
-    end
-  end
-end
