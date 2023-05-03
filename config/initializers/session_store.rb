c717439477479bb50faeed7ebfd3c638897cc4fe
Sane905 <konjaku0905@yahoo.co.jp>
2023-05-03 23:12:30 +0900

    fix:cookiestroeが使えるように

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,11 @@
 # config/initializers/session_store.rb
 
-Rails.application.config.middleware.insert_before Rack::Head, ActionDispatch::Cookies
-Rails.application.config.middleware.insert_after ActionDispatch::Cookies, ActionDispatch::Session::RedisStore,
-  servers: ['redis://redis:6379/0'],
-  expire_after: 2.weeks,
-  key: "_app_session"
+# Rails.application.config.middleware.insert_before Rack::Head, ActionDispatch::Cookies
+# Rails.application.config.middleware.insert_after ActionDispatch::Cookies, ActionDispatch::Session::RedisStore,
+#   servers: ['redis://redis:6379/0'],
+#   expire_after: 2.weeks,
+
+#   key: "_app_session"
+# Rails.application.config.session_store :active_record_store, key: '_mcn_session'
+Rails.application.config.session_store :cookie_store, key: '_mcn_session'
+
